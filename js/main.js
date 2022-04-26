428deb8ea2005861f42072a9dccc7025b4b44b37
Sandrine-dev <sandrine.volpi@gmail.com>
2022-04-26 19:03:47 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function openCheats(evt, cheats) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cheats).style.display = "block";
+  evt.currentTarget.className += " active";
+}
